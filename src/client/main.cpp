920a3e7a72a20dffcbb9f7f5466767d755433c3f
eltadj <eltadj.benmira@gmail.com>
2021-10-08 09:20:10 +0200

    police_jeu

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -21,5 +21,31 @@ int main(int argc,char* argv[])
 
     cout << "It works !" << endl;
 
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+
+
+
+        // Update the window
+        window.display();
+    }
+
+
     return 0;
 }

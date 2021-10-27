9679d71829fa9efbd2e0823df3725171ab5728e6
eltadj <eltadj.benmira@gmail.com>
2021-10-27 11:42:31 +0200

    update dia

---
+++ src/shared/state/jeu.cpp
@@ -0,0 +1,30 @@
+// my_class.cpp
+#include "Jeu.h" // header in local directory
+#include <iostream> // header in standard library
+#include <iostream>
+#include <string>
+using namespace state;
+using namespace std;
+
+void Jeu::tourSuivant()
+{
+    tour=tour+1
+
+}
+
+
+
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+
+Jeu ensea;
+
+  
+ ensea.tourSuivant()
+ 
+    return 0;
+}
---
+++ src/shared/state/testUni.cpp
@@ -0,0 +1,30 @@
+#include <boost/test/unit_test.hpp>
+
+#include "../../src/shared/state/jeu.h"
+#include "../../src/shared/state/cell.h"
+
+using namespace ::state;
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+  BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_CASE(TestJeu)
+{
+  {
+    //Jeu jeu;
+    //BOOST_CHECK_EQUAL(jeu.tour, 1);
+    //ex.setX(21);
+    //BOOST_CHECK_EQUAL(ex.x, 21);
+  }
+
+  {
+    //Jeu jeu;
+    //jeu.setX21);
+    //BOOST_CHECK_LE(ex.x, 32); // Less than equal
+    //BOOST_CHECK_GT(ex.x, 11); // Greater than equl
+  }
+}
+
+/* vim: set sw=2 sts=2 et : */

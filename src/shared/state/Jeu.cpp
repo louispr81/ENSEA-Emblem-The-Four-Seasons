f7fa7434f262498e88ba9670175b45c281b33bc1
eltadj <eltadj.benmira@gmail.com>
2021-11-01 15:25:26 +0100

    update dia

--- src/shared/state/Jeu.cpp
+++ src/shared/state/Jeu.cpp
@@ -1,33 +1,11 @@
 // my_class.cpp
 #include "Jeu.h" // header in local directory
 #include <iostream> // header in standard library
-
 #include <string>
 using namespace state;
 using namespace std;
 
 
- Jeu::Jeu(){
+ void Jeu::Jeu(){
         int tour=1;
-
-
 }
-
-void Jeu::tourSuivant()
-{
-    
-    cout << "tour suivant" << endl;
-
-};
-
-
-void fonction(int vie){
- int point= vie;
- string tour(" point:");
- cout<< tour << point;
- cout<<endl;
-}
-
-
-
-

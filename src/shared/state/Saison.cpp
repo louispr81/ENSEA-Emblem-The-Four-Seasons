e8bf2ca2852b5ea7f23d12f8d287ac9b152e8394
louis <pradines.louis@gmail.com>
2021-11-01 23:37:10 +0100

    correction bug et setter getter stats

--- src/shared/state/Saison.cpp
+++ src/shared/state/Saison.cpp
@@ -2,23 +2,24 @@
 #include "SaisonId.h"
 #include "Personnage.h"
 #include "Jeu.h"
+using namespace state;
 
 /*void Saison::Saison(std::string nom, std::list<int> bonus, SaisonId id)
 {    
     this->nom = nom;
     this->bonus = bonus;
     this->id = id;
-}*/
+}
 
 
 void Saison::updateStatsSaison(Personnage personnage)
 {
-    /*when the saison and the saison of the character are the same*/
+    /*when the saison and the saison of the character are the same
     if(personnage.getSaison().id == jeu.getSaison().id)  
     {
         personnage.setBonus(this->bonus);
     }
-    /*when the saison and the saison of the character are contrary*/
+    /*when the saison and the saison of the character are contrary
     else if(personnage.getSaison().saisonOppId == jeu.getSaison().id)
     {
         

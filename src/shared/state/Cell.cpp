eeca50a68452156eac2a4836459e6e5dd7074051
eltadj <eltadj.benmira@gmail.com>
2021-11-15 18:37:06 +0100

    update CEll

---
+++ src/shared/state/Cell.cpp
@@ -0,0 +1,41 @@
+#include "Cell.h"
+using namespace state;
+
+ Cell::Cell (Personnage* personnage, CellId type, std::vector<int> coordonees, bool walkable, int costPm, bool occupe){
+
+     //(*this).personnage=personnage;
+    // (*this).type=type;
+    // (*this).coordonees=coordonees;
+    // (*this).walkable=walkable;
+    // (*this).costPm=costPm;
+    // (*this).occupe=occupe;
+ }
+
+
+
+CellId Cell::getType (){
+    return (*this).type;
+}
+
+
+int Cell::getCostPm (){
+   return (*this).costPm;
+}
+
+
+void Cell::setCostPm (int costPm){
+    (*this).costPm=costPm;
+}
+
+void Cell::setPersonnage (Personnage* personnage){
+    (*this).personnage=personnage;
+}
+
+void Cell::setOccupe (bool occupe){
+    (*this).occupe=occupe;
+}
+
+void Cell::setWalkable (bool walkable){
+    (*this).walkable=walkable;
+
+}

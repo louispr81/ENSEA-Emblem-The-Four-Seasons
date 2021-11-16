96657fea1e6d19dea89d29cec22b5b0c1d6e127d
WANGZzzzzhao <56508259+WANGZzzzzhao@users.noreply.github.com>
2021-11-16 17:42:57 +0100

    Add files via upload

---
+++ src/client/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "../../src/shared/state.h"
+
+
+using namespace std;
+using namespace state;
+
+
+int main(int argc, char* argv[])
+{
+   state::State s;
+    
+
+
+    cout << "Hello World !" << endl;
+
+
+    return 0;
+}

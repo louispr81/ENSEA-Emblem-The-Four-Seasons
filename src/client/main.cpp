ce4ebea6bc8ff4b50f8a524f662c4706f78a244c
eltadj <eltadj.benmira@gmail.com>
2021-11-23 11:09:39 +0100

    update daplacer 23/11

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,18 +1,56 @@
 #include <iostream>
 #include "../../src/shared/state.h"
+#include "client.h"
+#include <unistd.h>
+#include <string.h> 
+using namespace std;
+using namespace state;
+//using namespace render;
+using namespace client;
 
 
 using namespace std;
 using namespace state;
 
 
+void usage(char * argv0){
+    cout << "Usage: " << argv0 << " COMMAND" << endl;
+    cout << "\tCOMMAND := { ";
+        cout << "hello" << " | ";
+        cout << "state";
+    cout << " }" << endl;
+}
+
 int main(int argc, char* argv[])
 {
-   state::State s;
+
+if (argc == 1){
+        usage(argv[0]);
+        std::exit(EXIT_FAILURE);
+    }
+
+
+if (strcmp(argv[1],"hello") ==0 ){
+        std::cout << "hello World " << std::endl;
+    }
+	else if (strcmp(argv[1],"state") == 0){
+        state::State state;
+
+		std::cout << "CREATION DE STATE" << std::endl;
+    }
+
+    else if (strcmp(argv[1],"render") == 0){
+		std::cout << "RENDER" << std::endl;
+	}
     
+    else {
+        std::cout << "Wrong command !" << std::endl ;
+    }
 
 
-    cout << "Hello World !" << endl;
+   
+    
+   
 
 
     return 0;

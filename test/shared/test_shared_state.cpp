0bfab4cc9e326e61802e9502450b7d266083bcdb
wangzhao <wz909529414@gmail.com>
2021-11-23 18:04:06 +0100

    debug

--- test/shared/test_shared_state.cpp
+++ test/shared/test_shared_state.cpp
@@ -18,7 +18,7 @@ BOOST_AUTO_TEST_CASE(TestStateNamespace)
     BOOST_CHECK_EQUAL(jeuTest.getTour(), 1);
     jeuTest.tourSuivant();
     BOOST_CHECK_EQUAL(jeuTest.getTour(), 2);
-    jeuTest.updateSaison();
+    
     /*
     for(Saison* i:jeuTest.getListeSaison()){
       std::cout<<i<<'\n';
@@ -28,18 +28,18 @@ BOOST_AUTO_TEST_CASE(TestStateNamespace)
       std::cout<<(*i).getSaisonOppId()<<'\n';
     }*/
    // std::cout<<"pointer"<<'\n';
-    //Saison* pntSaison= jeuTest.getSaison();
-    //BOOST_CHECK_EQUAL((*pntSaison).getId(), PRINTEMPS);
-   /* jeuTest.updateSaison();
+    jeuTest.updateSaison();   
+    BOOST_CHECK_EQUAL((*(jeuTest.getSaison())).getId(), ETE);
+    jeuTest.updateSaison();
     BOOST_CHECK_EQUAL((*(jeuTest.getSaison())).getId(), AUTOMNE);
     jeuTest.updateSaison();
     BOOST_CHECK_EQUAL((*(jeuTest.getSaison())).getId(), HIVER);
-    jeuTest.updateSaison();
-    BOOST_CHECK_EQUAL((*(jeuTest.getSaison())).getId(), PRINTEMPS);  */                  
+   // jeuTest.updateSaison();
+   // BOOST_CHECK_EQUAL((*(jeuTest.getSaison())).getId(), PRINTEMPS);                    
 
 
     //Test gameover
-    jeuTest.abandonner(jeuTest.getJoueurs(0));
+    jeuTest.abandonner(*jeuTest.getJoueurs(0));
     BOOST_CHECK_EQUAL(jeuTest.getGameover(), true);
     //ex.setX(21);
     //BOOST_CHECK_EQUAL(ex.x, 21);*/

7752da8ae4daceefca494f2169440098691c3c49
wangzhao <wz909529414@gmail.com>
2022-01-07 20:23:39 +0100

    upd

--- src/shared/moteur/CommandAttendre.cpp
+++ src/shared/moteur/CommandAttendre.cpp
@@ -11,13 +11,7 @@ CommandAttendre::CommandAttendre(state::State* currentState){
   this->id = ATTENDRE;
   this->currentState = currentState;
 }
-/*
-Json::Value CommandAttendre::serialize(){
-    Json::Value newCommand;
-    newCommand["id"]=id;
-    return newCommand;
-}
-*/
+
 void CommandAttendre::execute(){
     currentState->getPersonnageActif()->attendre();
 }

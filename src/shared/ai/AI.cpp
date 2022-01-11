52b9515d35a89007b14c30e03992cd94f42158e1
louis <pradines.louis@gmail.com>
2022-01-11 10:31:27 +0100

    AI debug

---
+++ src/shared/ai/AI.cpp
@@ -0,0 +1,12 @@
+#include "AI.h"
+#include <iostream>
+
+using namespace ai;
+
+AI::AI(){
+
+}
+
+AI::~AI(){
+    
+}
--- src/shared/ai/RandomAI.cpp
+++ src/shared/ai/RandomAI.cpp
@@ -12,11 +12,16 @@ RandomAI::RandomAI(){
  
 void RandomAI::generateCommand(){
     //[a,b]  (rand() % (b-a+1))+ a 
-    this->commandId=(rand() % (engine->getListCommand().size()+1));    
+    cout<<"1"<<std::endl;
+    this->commandId=(rand() % (engine->getListCommand().size()));    
+    cout<<"2"<<std::endl;
     this->moveId=(rand() % 5); 
+    cout<<"3"<<std::endl;
     if(this->commandId == 0 || this->commandId == 1){
+        cout<<"4"<<std::endl;
         this->moveId=(rand() % 4)+1;
     }       
+    cout<<"IA OK"<<std::endl;
 }
 int RandomAI::getCommandId(){
     return this->commandId;

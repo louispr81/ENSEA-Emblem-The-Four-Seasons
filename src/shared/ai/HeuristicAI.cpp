be334a5ba44081a4ad0a42a0a38d3e94b1580a18
wangzhao <wz909529414@gmail.com>
2022-01-14 14:32:05 +0100

    heuristic ai

--- src/shared/ai/HeuristicAI.cpp
+++ src/shared/ai/HeuristicAI.cpp
@@ -23,8 +23,6 @@ void HeuristicAI::generateCommand(){
     int distanceXmin,distanceYmin;
     int distanceMinIndex;
     joueurEnemy = state->getJoueurs()[((state->getJoueur()->getId()-40)+1)%2];
-
-    commandId = 1; // move
     position = state->getPersonnageActif()->getCell()->getCoordonees();
     for(int i=0;i<joueurEnemy->getPersonnages().size();i++){
         if(joueurEnemy->getPersonnages()[i]->getAlive()==true){
@@ -33,17 +31,17 @@ void HeuristicAI::generateCommand(){
             positionEnemy = joueurEnemy->getPersonnages()[i]->getCell()->getCoordonees();
             distanceX = abs(positionEnemy[0]-position[0]);
             distanceY = abs(positionEnemy[1]-position[1]);
-            if((positionEnemy[0]-position[0])>0){
-                signeX = 1;
+            if((positionEnemy[0]-position[0])>0){  
+                signeX = 1;  // x, left
             }
             else{
-                signeX = -1;
+                signeX = -1; // x, right
             }
             if((positionEnemy[1]-position[1])>0){
-                signeY = 1;
+                signeY = 1;  // y, down
             }
             else{
-                signeY = -1;
+                signeY = -1; // y, up 
             }
             distance = sqrt(pow(distanceX,2)+pow(distanceY,2));
             distanceEnemy.push_back(distance);
@@ -59,16 +57,48 @@ void HeuristicAI::generateCommand(){
 
     int pm;
     pm=state->getPersonnageActif()->getStatistic()->getPoint_mouvement();
-    
-    while(pm>0){
-        if(distanceXmin>distanceYmin){
-            
+    if(distanceXmin == 1){
+        commandId = 1; //attack
+        if(signeX == 1){
+            moveId = 2;
+        }
+        else{
+            moveId = 1;
         }
     }
-    
-    //state->getPersonnageActif()->deplacer((position[0]+(signeX)*distanceXmin),(position[1]+(signeY)*distanceYmin));
-
-
+    else if(distanceYmin == 1){
+        commandId = 1;
+        if(signeY == 1){
+            moveId = 4;
+        }
+        else{
+            moveId = 3;
+        }
+    }
+    else if(distanceXmin>1 && distanceYmin>1){
+        commandId = 1; // move 
+        if(pm > 0){
+            if(distanceXmin>distanceYmin && signeX==1 ){  // x > y, dx > 0
+                moveId = 2;//RIGHT;
+                pm = pm-1;
+            }
+            if(distanceXmin>distanceYmin && signeX==-1 ){  // x > y, dx < 0
+                moveId = 1;//LEFT;
+                pm = pm-1;
+            }
+            if(distanceXmin<distanceYmin && signeX==1 ){  // x < y, dx > 0
+                moveId = 4;//DOWN;
+                pm = pm-1;
+            }
+            if(distanceXmin<distanceYmin && signeX==-1 ){  // x < y, dx < 0
+                moveId = 3;//UP;
+                pm = pm-1;
+            }
+        }
+        else{
+            commandId = 2; //attendre
+        }
+    }
 }
 int HeuristicAI::getMoveId(){
     return moveId;
